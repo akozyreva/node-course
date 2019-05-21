f09afe6e70a5a50a28ac445a4600fffede15a837
akozyreva <anna.ko4523546@gmail.com>
2019-05-21 16:18:45 +0200

    add ability to use prompt for running service

--- 01-weather-app/app.js
+++ 01-weather-app/app.js
@@ -1,19 +1,25 @@
+// run it by the following command
+// node app.js Moscow
 const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geoCode('Frankfurt am Main', (error, data) => {
-  if(error) {
-    return console.log(error)
-  }
-  // console.log('Data', data)
-  // forecastData is another data, so we need to separate names for geoCode and forecast
-  forecast(data.latitude, data.longtitude, (error, forecastData) => {
-      if(error) {
-        return console.log(error)
-      }
-      // we have no ability to recieve a location from forecast, so
-      // take it from geocode answer
-      console.log(data.location)
-      console.log(forecastData)
+if(process.argv.length < 3) {
+  console.log("Specify city")
+} else {
+  geoCode(process.argv[2], (error, data) => {
+    if(error) {
+      return console.log(error)
+    }
+    // console.log('Data', data)
+    // forecastData is another data, so we need to separate names for geoCode and forecast
+    forecast(data.latitude, data.longtitude, (error, forecastData) => {
+        if(error) {
+          return console.log(error)
+        }
+        // we have no ability to recieve a location from forecast, so
+        // take it from geocode answer
+        console.log(data.location)
+        console.log(forecastData)
+    })
   })
-})
+}
