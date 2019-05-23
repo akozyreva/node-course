6febeab03e6791cee04f275bc0f61f2d6cab1a13
akozyreva <anna.ko4523546@gmail.com>
2019-05-23 15:56:51 +0200

    add hbs structure

--- 02-web-server/src/app.js
+++ 02-web-server/src/app.js
@@ -1,8 +1,10 @@
-// nodemon src/app.js
+// nodemon src/app.js - js,hbs
 // http://localhost:3000/
 
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
 
 console.log(__dirname)
 console.log(__filename)
@@ -11,12 +13,23 @@ console.log(path.join(__dirname, '../public'))
 
 // initialize app
 const app = express()
-const publiDir = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publiDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlbars and viees location
 // we enable hbs module, which we've installed before
 app.set('view engine', 'hbs')
+// here we specify viewPath for hbs files
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
+// Setup static dir to seve
 app.use(express.static(publiDir))
+
+
 
 // for rendering template we need to add router and render inside of it
 app.get('', (req, res) => {
@@ -37,6 +50,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
   res.render('help', {
     msg: 'Help msg',
+    title: 'Help',
+    name: "Anna"
   })
 })
 
