9d26bb1bd959cceb3851f7b70cf503c7776b3b0e
akozyreva <anna.ko4523546@gmail.com>
2019-06-03 15:25:42 +0200

    add fetching forecast to use it on front end side

--- 02-web-server/public/js/app.js
+++ 02-web-server/public/js/app.js
@@ -1,1 +1,16 @@
 console.log('Client Side js file is loading')
+
+fetch('http://puzzle.mead.io/puzzle').then( (res) => {
+    res.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=Moscow').then( res => {
+    res.json().then(data => {
+        //console.log(data)
+        console.log(data.location)
+        console.log(data.forecastData)
+    }).catch( er => console.log(`Error with retrieving data ${er}`))
+}).catch(er => console.log(`Problem with senging req ${er}`))
+
