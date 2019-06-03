9464ec24439080896274c9edb42176b23432f54a
akozyreva <anna.ko4523546@gmail.com>
2019-06-03 15:43:30 +0200

    fetching specific forecast from input

--- 02-web-server/public/js/app.js
+++ 02-web-server/public/js/app.js
@@ -6,11 +6,23 @@ fetch('http://puzzle.mead.io/puzzle').then( (res) => {
     })
 })
 
-fetch('http://localhost:3000/weather?address=Moscow').then( res => {
-    res.json().then(data => {
-        //console.log(data)
-        console.log(data.location)
-        console.log(data.forecastData)
-    }).catch( er => console.log(`Error with retrieving data ${er}`))
-}).catch(er => console.log(`Problem with senging req ${er}`))
 
+const weatherForm = document.querySelector('form')
+
+
+weatherForm.addEventListener('submit', (event) => {
+    // it's for not reloading page after submit btn
+    event.preventDefault()
+    // retrieve val from input
+    const search = document.querySelector('input').value
+    if(!search) {
+        return
+    }
+    fetch(`http://localhost:3000/weather?address=${search}`).then( res => {
+        res.json().then(data => {
+            //console.log(data)
+            console.log(data.location)
+            console.log(data.forecastData)
+        }).catch( er => console.log(`Error with retrieving data ${er}`))
+    }).catch(er => console.log(`Problem with senging req ${er}`))
+})
