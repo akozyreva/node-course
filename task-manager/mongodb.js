9b525b6d3afbde88f6b6caaca3d00c20ccf92318
akozyreva <anna.ko4523546@gmail.com>
2019-06-14 11:44:47 +0200

    adding queries for searching documantes

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,20 +1,9 @@
 // CRUD create read update delete
 
-/*const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const ObjectID = mongodb.ObjectID */
-
-// we can write the same, but with destructuring and in one line
-// ObjectID is used for generating id without fetching queries to the server
 const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databseName = 'task-manager'
-
-const id = new ObjectID()
-console.log(id)
-// it has a method, which returns timestamp
-console.log(id.getTimestamp())
 
 // connect to the client
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (er, client) => {
@@ -26,64 +15,39 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (er, client) => {
    // create new db if it doesn't exist
    const db = client.db(databseName)
 
-   /*db.collection('users').insertOne({
-       name: 'Anna', 
-       age: 26
-   }, (er, res) => {
-       if(er) {
-           return console.log("Unable to insert user")
-       }
-       // it returns back the successful result - documentation, which was inserted
-       console.log(res.ops)
-   }) 
-
-   db.collection('users').insertMany([
-       {
-         name: 'Jane',
-         age: 28
-       },
-       {
-        name: 'Gunter',
-        age: 27
-       }
-   ], (er, res) => {
-        if(er) {
-            return console.log("Unable to insert documentation")
-        }
-
-        console.log(res.ops)
+   // find individual document
+   db.collection('users').findOne({name: 'Anna', age: 1}, (err, user) => {
+    if (err) {
+        return console.log('Unable to fetch')
+    } 
+    console.log(user)
    })
-   
-  db.collection('tasks').insertMany([
-      {
-          description: 'test',
-          completed: true
-      },
-      { 
-        description: 'test2',
-        completed: true
-      },
-      {
-        description: 'test3',
-        completed: true
-      }
-  ], (er, res) => {
-      if(er) {
-          return console.log("Unable to create tasks")
-      } 
-      console.log(res.ops)
-  })
-   */
-  db.collection('users').insertOne({
-      // and we reference for rahdomly generating id
-    _id: id,
-    name: 'Max', 
-    age: 27
-}, (er, res) => {
-    if(er) {
-        return console.log("Unable to insert user")
-    }
-    // it returns back the successful result - documentation, which was inserted
-    console.log(res.ops)
-}) 
+
+   // how to find queries, using id
+   db.collection('users').findOne({"_id" : new ObjectID("5d03623f70d9431b0800e85c")}, (err, user) => {
+    if (err) {
+        return console.log('Unable to fetch')
+    } 
+    console.log(user)
+   })
+
+   // how to find - return a cursor, which provide toArray method to return an array
+   db.collection('users').find({name: 'Anna'}).toArray( (err, users) => {
+       console.log(users)
+   })
+
+   // returns the count of occurencies
+   db.collection('users').find({name: 'Anna'}).count( (err, count) => {
+    console.log(count)
+    })
+
+    // find the last task
+    db.collection('tasks').find().limit(1).sort({_id : 1}).toArray( (err, task) => {
+        console.log(task)
+    })
+    
+    // find the tasks, that aren't completed
+    db.collection('tasks').find({completed: true}).toArray( (err, tasks) => {
+        console.log(tasks)
+    })
 })
