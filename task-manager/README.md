10a7dbc99d4425e377bd5f89417c811db555e25c
akozyreva <anna.ko4523546@gmail.com>
2019-06-14 11:11:00 +0200

    lection about ObjectID

---
+++ task-manager/README.md
@@ -0,0 +1,5 @@
+for initializing mong
+ /Users/akozyreva/mongodb/bin/mongod --dbpath=/Users/akozyreva/mongodb-data/
+
+for running db script
+node mongodb.js 
--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,20 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
+/*const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID */
+
+// we can write the same, but with destructuring and in one line
+// ObjectID is used for generating id without fetching queries to the server
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+// it has a method, which returns timestamp
+console.log(id.getTimestamp())
 
 // connect to the client
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (er, client) => {
@@ -43,7 +53,7 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (er, client) => {
 
         console.log(res.ops)
    })
-   */
+   
   db.collection('tasks').insertMany([
       {
           description: 'test',
@@ -63,5 +73,17 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (er, client) => {
       } 
       console.log(res.ops)
   })
-   
+   */
+  db.collection('users').insertOne({
+      // and we reference for rahdomly generating id
+    _id: id,
+    name: 'Max', 
+    age: 27
+}, (er, res) => {
+    if(er) {
+        return console.log("Unable to insert user")
+    }
+    // it returns back the successful result - documentation, which was inserted
+    console.log(res.ops)
+}) 
 })
